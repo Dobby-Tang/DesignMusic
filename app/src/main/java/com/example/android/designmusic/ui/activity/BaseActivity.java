690e9e53f3cc502857797b7958aa437a2d16f3f8
tpj <514820184@qq.com>
2016-04-14 20:34:08 +0800

    增加BaseActivity

---
+++ app/src/main/java/com/example/android/designmusic/ui/activity/BaseActivity.java
@@ -0,0 +1,29 @@
+package com.example.android.designmusic.ui.activity;
+
+import android.app.Activity;
+
+/**
+*@author By Dobby Tang
+*Created on 2016-04-14 20:33
+*/
+public class BaseActivity extends Activity{
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
